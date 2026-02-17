0c7735c8bcfae3b0fc65361e6cbaa0f7f134b190
Musau Vin <musauvinnie@outlook.com>
2026-02-17 10:01:46 +0300

    added css componets

--- src/stores/posts.js
+++ src/stores/posts.js
@@ -6,6 +6,7 @@ export const usePostsStore = defineStore('postsStore', {
         return {
             posts: [],
             categories: [],
+            selectedCategoryId: null,
             errors: {},
 
         }
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -54,15 +54,22 @@ onMounted(() =>  {
             {{ cat.name }}
           </option>
         </select>
-          <input 
-            type="text" 
-            v-model="searchQuery" 
-            placeholder="Search titles..." 
-            class="custom-select search-bar"
-          />
-          <button v-if="searchQuery" @click="searchQuery = ''" class="clear-btn">
-            &times;
-          </button>
+          <div class="search-container">
+            <span class="search-icon">
+              <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="11" cy="11" r="8"/><line x1="21" y1="21" x2="16.65" y2="16.65"/></svg>
+            </span>
+            
+            <input 
+              type="text" 
+              v-model="searchQuery" 
+              placeholder="Search titles..." 
+              class="custom-select search-bar"
+            />
+
+            <button v-if="searchQuery" @click="searchQuery = ''" class="clear-btn">
+              &times;
+            </button>
+          </div>
       </div>
 
       <div class="post-feed">
